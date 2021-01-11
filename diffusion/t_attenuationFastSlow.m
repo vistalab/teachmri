2b26fe82554e0a42f0f2e980ad950c5eec2250f1
Brian Wandell <wandell@stanford.edu>
2021-01-10 20:40:38 -0800

    More to do on the diffusion simulations

--- diffusion/t_attenuationFastSlow.m
+++ diffusion/t_attenuationFastSlow.m
@@ -3,27 +3,43 @@
 % Le Bihan and Johansen-Berg Neuroimage after 25 years article
 % 
 % The give this formula for diffusion fast and slow and cite work from a
-% rat model. It would be good  to see how agrees and disagrees with that.
+% rat model. It would be good  to see who agrees and disagrees with that.
 % It would also be good to see if Ariel had any data on this - diffusion
 % signal as a function of b value (probably gradient strength)
 %
-% Useful for people in Psych 204A to work on the data and ask how well the
+% Useful for people in Psych 204A to work on real data and ask how well the
 % bi-exponential does.  Le Bihan is very confident.  But as I recall Ariel
-% and the girl who worked with him had a hard time seeing the
+% and the person who worked with him had a hard time seeing the
 % bi-exponential.
 %
-b = linspace(800,3000,10);  % The range in our human experiments
+% The difference between the single and bi-exponential model is really
+% whether there is a single line fit or not.  And I don't think we can
+% tell.
+%
+
+%%  Mean signal of the two-exponential model
+
+b = linspace(800,3000,10);  % The b-value range in our human experiments
 D1 = 1.68e-4; f1 = 0.17;    % Slow
 D2 = 8.24e-4; f2 = 0.8;     % Fast
 
+% The signal attention of the model
 a = f1*exp(-b.*D1) + f2*exp(-b.*D2);
+a0 = f1*exp(-b.*D1);
+
+a = a/max(a(:));
+a0 = a0/max(a0(:));
 
 vcNewGraphWin;
-semilogx(b,a);
+% 
+semilogy(b,a,'r-',b,a0,'k--');
 grid on
+xlabel('b-value')
+ylabel('Relative attention (re: 800)');
 
-%% What if we have some noise, say 2% of the signal level
-pErr = 0.05;
+%% What if we have some noise, say 3% of the signal level
+
+pErr = 0.03;
 nSamp = 2;
 aNoise = zeros(numel(a),nSamp);
 for ii=1:length(b)
@@ -33,8 +49,12 @@ for ii=1:length(b)
 end
 
 vcNewGraphWin;
-for ii=1:size(aNoise,1);
-    plot(b(ii)*ones(nSamp,1),aNoise(ii,:));
+for ii=1:size(aNoise,1)
+    semilogy(b(ii)*ones(nSamp,1),aNoise(ii,:),b,a,'k--','LineWidth',2);
     hold on
 end
 grid on
+xlabel('b-value'); ylabel('Mean signal');
+
+
+%%

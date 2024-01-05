424430ee16ec70e2d16000d57d99b150e725b080
Brian Wandell <1837145+wandell@users.noreply.github.com>
2024-01-05 10:45:33 -0800

    Update v_mrTutorials.m

--- tutorials/validation/v_mrTutorials.m
+++ tutorials/validation/v_mrTutorials.m
@@ -1,12 +1,15 @@
 %% Execute the Psych 204A tutorials
 % 
+% These are all live scripts now.
+%
+% Maybe put this in ieValidate?
 %
 % Wandell, 2019
 
 %% Will add more validation tests within later
 
 chdir(fullfile(mrTeachRootPath,'tutorials'));
-files = dir('tls_*.mlx');
+files = dir('t_*.mlx');
 for ii=7:numel(files)
     fprintf('Validating %s ',files(ii).name);
     run(files(ii).name);
